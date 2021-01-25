24f7cfc65d2a7ccf8b4943b55a4b4032c77ea992
Philippe DRABCZUK <philippe.drabczuk@mappy.com>
2021-01-25 15:02:51 +0100

    converting routeData

--- Investigations/converter.cpp
+++ Investigations/converter.cpp
@@ -8,6 +8,7 @@
 
 #include "../DataStructures/RAPTOR/Entities/Stop.h"
 #include "../DataStructures/RAPTOR/Entities/Route.h"
+#include "../Custom/gtfs.h"
 
 using namespace std;
 
@@ -30,6 +31,15 @@ vector<RAPTOR::Stop> convert_stopData(ad::cppgtfs::gtfs::Feed const& feed) {
     return stopData;
 }
 
+vector<RAPTOR::Route> convert_routeData(
+    std::map<my::StopSetId, std::unordered_set<my::TripId> > const& stopsetToTrips) {
+    vector<RAPTOR::Route> routeData;
+    routeData.reserve(stopsetToTrips.size());
+    transform(stopsetToTrips.begin(), stopsetToTrips.end(), back_inserter(routeData),
+              [](auto& stopset_to_trip) { return RAPTOR::Route(stopset_to_trip.first); });
+    return routeData;
+}
+
 int main(int argc, char** argv) {
     if (argc < 2)
         usage();
@@ -46,13 +56,24 @@ int main(int argc, char** argv) {
     cout << "Le feed contient " << feed.getStops().size() << " stops" << endl;
     cout << "Le feed contient " << feed.getTrips().size() << " trips" << endl;
 
+    // stopData :
     vector<RAPTOR::Stop> stopData = convert_stopData(feed);
-    ;
     cout << "À ce stade, stopData contient : " << stopData.size() << " items." << endl;
     int counter = 0;
     for (auto& stop : stopData) {
-        if (counter++ <= 14) {
+        if (counter++ <= 8) {
             cout << "STOP = " << stop << endl;
+        }
+    }
+
+    // routeData :
+    auto[stopsetToTrips, routesToStopsets] = my::partition_trips_in_stopsets(feed);
+    vector<RAPTOR::Route> routeData = convert_routeData(stopsetToTrips);
+    cout << "À ce stade, routeData contient : " << routeData.size() << " items." << endl;
+    counter = 0;
+    for (auto& route : routeData) {
+        if (counter++ <= 8) {
+            cout << "ROUTE = " << route << endl;
         }
     }
 
