75241bafd92feb21c48784adfe964e368086f216
Philippe DRABCZUK <philippe.drabczuk@mappy.com>
2021-01-25 14:23:13 +0100

    building stopData is now a function

--- Investigations/converter.cpp
+++ Investigations/converter.cpp
@@ -16,11 +16,25 @@ inline void usage() noexcept {
     exit(1);
 }
 
+vector<RAPTOR::Stop> convert_stopData(ad::cppgtfs::gtfs::Feed const& feed) {
+    vector<RAPTOR::Stop> stopData;
+    for (auto const & [ stop_id, stop_ptr ] : feed.getStops()) {
+        if (stop_ptr == 0) {
+            cout << "ERROR : stop_ptr is 0 for id : " << stop_id << endl;
+            exit(1);
+        }
+        auto const& stop = *stop_ptr;
+        Geometry::Point location{Construct::LatLongTag{}, stop.getLat(), stop.getLng()};
+        stopData.emplace_back(stop.getName(), location);
+    }
+    return stopData;
+}
+
 int main(int argc, char** argv) {
     if (argc < 2)
         usage();
 
-    const std::string gtfs_folder = argv[1];
+    const string gtfs_folder = argv[1];
 
     cout << "Parsing GTFS from folder : " << gtfs_folder << endl;
 
@@ -32,27 +46,15 @@ int main(int argc, char** argv) {
     cout << "Le feed contient " << feed.getStops().size() << " stops" << endl;
     cout << "Le feed contient " << feed.getTrips().size() << " trips" << endl;
 
-    std::vector<RAPTOR::Stop> stopData;
-
-    int counter = 0;
-    for (auto const & [ stop_id, stop_ptr ] : feed.getStops()) {
-        if (stop_ptr == 0) {
-            cout << "ERROR : stop_ptr is 0 for id : " << stop_id << endl;
-            exit(1);
-        }
-        auto const& stop = *stop_ptr;
-        if (counter++ <= 14) {
-            cout << "STOP = " << stop.getId() << endl;
-            cout << "\t " << stop.getName() << endl;
-            cout << "\t " << stop.getLat() << endl;
-            cout << "\t " << stop.getLng() << endl;
-        }
-
-        Geometry::Point location{Construct::LatLongTag{}, stop.getLat(), stop.getLng()};
-        stopData.emplace_back(stop.getName(), location);
-    }
-
+    vector<RAPTOR::Stop> stopData = convert_stopData(feed);
+    ;
     cout << "À ce stade, stopData contient : " << stopData.size() << " items." << endl;
+    int counter = 0;
+    for (auto& stop : stopData) {
+        if (counter++ <= 14) {
+            cout << "STOP = " << stop << endl;
+        }
+    }
 
     return 0;
 }
