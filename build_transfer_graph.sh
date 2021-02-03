86c1bff38c775d7fe21dcfe6b51006172425ba0d
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-02-03 15:59:59 +0100

    ok to build

--- build_transfer_graph.sh
+++ build_transfer_graph.sh
@@ -16,18 +16,14 @@ echo "To build from scratch :  rm -rf '$BUILD_DIR'"
 
 pushd "$CMAKE_ROOT_DIR"
 mkdir -p "$BUILD_DIR"
-conan install --install-folder="$BUILD_DIR" . --profile="${CMAKE_ROOT_DIR}/conanprofile.txt"
+conan install --install-folder="$BUILD_DIR" .
 cmake -B"$BUILD_DIR" -H"$CMAKE_ROOT_DIR"
 make -j -C "$BUILD_DIR" build-transfer-graph
 popd
 
-
-# the used compiler is a "local" one (system's clang is not c++17 compliant) :
-CLANG_LIBS="/home/pdrabczuk/Logiciels/clang+llvm-9.0.1-x86_64-linux-gnu-ubuntu-16.04/lib"
-
 # run server :
 WORKDIR="${this_script_parent}/WORKDIR"
 echo "Using data from WORKDIR = $WORKDIR"
-LD_LIBRARY_PATH="${CLANG_LIBS}" "${BUILD_DIR}/bin/build-transfer-graph" \
+"${BUILD_DIR}/bin/build-transfer-graph" \
     "pouet1" \
     "pouet2"
