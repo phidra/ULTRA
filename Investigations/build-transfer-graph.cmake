291af4a32a3d44529c6defc4e0e9634116a3e8ce
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-02-04 11:28:09 +0100

    simplifying use of custom sources

--- Investigations/build-transfer-graph.cmake
+++ Investigations/build-transfer-graph.cmake
@@ -1,5 +1,5 @@
 set(BUILD-TRANSFER-GRAPH build-transfer-graph)
 
-add_executable(${BUILD-TRANSFER-GRAPH} build-transfer-graph.cpp ../Custom/Common/polygon.cpp ../Custom/Parsing/polygonfile.cpp ../Custom/Graph/graph.cpp ../Custom/Graph/osmparsing.cpp ../Custom/Graph/extending_with_stops.cpp)
+add_executable(${BUILD-TRANSFER-GRAPH} build-transfer-graph.cpp ${CUSTOM_SOURCES})
 target_link_libraries(${BUILD-TRANSFER-GRAPH} bz2 z expat -pthread)
 target_include_directories(${BUILD-TRANSFER-GRAPH} PUBLIC ${CMAKE_SOURCE_DIR})
