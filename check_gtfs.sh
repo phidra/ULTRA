f846b39eb980cdd7fa960103c7f5918ef27c9fd6
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-02-04 16:12:22 +0100

    using clang as compiler

--- check_gtfs.sh
+++ check_gtfs.sh
@@ -17,7 +17,7 @@ echo "To build from scratch :  rm -rf '$BUILD_DIR'"
 # building :
 pushd "$CMAKE_ROOT_DIR"
 mkdir -p "$BUILD_DIR"
-conan install --install-folder="$BUILD_DIR" .
+conan install --install-folder="$BUILD_DIR" . --profile="conanprofile.txt"
 cmake -B"$BUILD_DIR" -H"$CMAKE_ROOT_DIR"
 make -j -C "$BUILD_DIR" gtfs-checker
 popd
--- run_server.sh
+++ run_server.sh
@@ -16,7 +16,7 @@ echo "To build from scratch :  rm -rf '$BUILD_DIR'"
 
 pushd "$CMAKE_ROOT_DIR"
 mkdir -p "$BUILD_DIR"
-conan install --install-folder="$BUILD_DIR" .
+conan install --install-folder="$BUILD_DIR" . --profile="conanprofile.txt"
 cmake -B"$BUILD_DIR" -H"$CMAKE_ROOT_DIR"
 make -j -C "$BUILD_DIR" ultra-server
 popd
