4f187cced3c0613e5c5bbd25f65a2ebf4b2b182e
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-02-23 17:10:05 +0100

    adding program to build raptor binary

---
+++ Investigations/build-raptor-binary.cmake
@@ -0,0 +1,5 @@
+set(BUILD-RAPTOR-BINARY build-raptor-binary)
+
+add_executable(${BUILD-RAPTOR-BINARY} build-raptor-binary.cpp ${CUSTOM_SOURCES})
+target_link_libraries(${BUILD-RAPTOR-BINARY} bz2 z expat -pthread)
+target_include_directories(${BUILD-RAPTOR-BINARY} PUBLIC ${CMAKE_SOURCE_DIR})
---
+++ Investigations/build-raptor-binary.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+#include "../DataStructures/RAPTOR/Data.h"
+#include "../DataStructures/Geometry/Point.h"
+#include "../Custom/Parsing/polygonfile.h"
+#include "../Custom/Parsing/gtfs_stops.h"
+#include "../Custom/Graph/extending_with_stops.h"
+#include "../Custom/Graph/graph.h"
+#include "../Custom/transfer_graph.h"
+#include "../Custom/Dumping/geojson.h"
+
+int main(int argc, char** argv) {
+    std::cout << "stub" << std::endl;
+    return 0;
+}
