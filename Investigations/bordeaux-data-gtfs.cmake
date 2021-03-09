2a779ff50c35a5db5223e7258fa09178a5dee623
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-03-09 10:07:37 +0100

    cleaner download cmake scripts

--- Investigations/bordeaux-data-gtfs.cmake
+++ Investigations/bordeaux-data-gtfs.cmake
@@ -1,15 +1,17 @@
 include(ExternalProject)
 
 set(BORDEAUX_GTFS_DESTINATION_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../DOWNLOADED_DATA/gtfs_bordeaux")
+set(BORDEAUX_GTFS_DOWNLOAD_URL "https://www.data.gouv.fr/fr/datasets/r/13e7e219-b037-4d60-a3ab-e55d2d3e5291")
 
-message(STATUS "Bordeaux GTFS download URL = '${BORDEAUX_GTFS_DESTINATION_DIRECTORY}'")
+message(STATUS "Bordeaux GTFS download URL       = '${BORDEAUX_GTFS_DOWNLOAD_URL}'")
+message(STATUS "Bordeaux GTFS download directory = '${BORDEAUX_GTFS_DESTINATION_DIRECTORY}'")
 
 # NOTE : it seems that if the target directory exists, the external project is not downloaded again
 
 ExternalProject_Add(
     gtfs_bordeaux
     PREFIX gtfs_bordeaux
-    URL "https://www.data.gouv.fr/fr/datasets/r/13e7e219-b037-4d60-a3ab-e55d2d3e5291"
+    URL "${BORDEAUX_GTFS_DOWNLOAD_URL}"
     DOWNLOAD_NAME "gtfs_bordeaux.zip"
     DOWNLOAD_DIR "${BORDEAUX_GTFS_DESTINATION_DIRECTORY}"
     SOURCE_DIR "${BORDEAUX_GTFS_DESTINATION_DIRECTORY}"
--- Investigations/bordeaux-data-osm.cmake
+++ Investigations/bordeaux-data-osm.cmake
@@ -1,15 +1,17 @@
 include(ExternalProject)
 
 set(BORDEAUX_OSM_DESTINATION_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/../DOWNLOADED_DATA/osm_bordeaux")
+set(BORDEAUX_OSM_DOWNLOAD_URL "https://download.geofabrik.de/europe/france/aquitaine-latest.osm.pbf")
 
-message(STATUS "Bordeaux OSM download URL = '${BORDEAUX_OSM_DESTINATION_DIRECTORY}'")
+message(STATUS "Bordeaux OSM download URL       = '${BORDEAUX_OSM_DOWNLOAD_URL}'")
+message(STATUS "Bordeaux OSM download directory = '${BORDEAUX_OSM_DESTINATION_DIRECTORY}'")
 
 # NOTE : it seems that if the target directory exists, the external project is not downloaded again
 
 ExternalProject_Add(
     osm_bordeaux
     PREFIX osm_bordeaux
-    URL "https://download.geofabrik.de/europe/france/aquitaine-latest.osm.pbf"
+    URL "${BORDEAUX_OSM_DOWNLOAD_URL}"
     DOWNLOAD_NO_EXTRACT TRUE
     DOWNLOAD_DIR "${BORDEAUX_OSM_DESTINATION_DIRECTORY}"
     SOURCE_DIR "${BORDEAUX_OSM_DESTINATION_DIRECTORY}"
