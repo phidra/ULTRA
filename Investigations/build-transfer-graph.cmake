c4047dfc173be72437afcfb93f3b0c5f64097f69
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-03-09 09:46:51 +0100

    remobing now unused binary

--- Investigations/build-transfer-graph.cmake
+++
@@ -1,7 +0,0 @@
-set(BUILD-TRANSFER-GRAPH build-transfer-graph)
-
-add_executable(${BUILD-TRANSFER-GRAPH} build-transfer-graph.cpp ${CUSTOM_SOURCES})
-target_link_libraries(${BUILD-TRANSFER-GRAPH} ad_cppgtfs)
-target_link_libraries(${BUILD-TRANSFER-GRAPH} bz2 z expat -pthread)
-target_include_directories(${BUILD-TRANSFER-GRAPH} PUBLIC "${CPPGTFS_INCLUDE_DIR}")
-target_include_directories(${BUILD-TRANSFER-GRAPH} PUBLIC ${CMAKE_SOURCE_DIR})
