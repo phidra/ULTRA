069ac989c1d4e08c2dfeeb1e7862f79b125bb076
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-03-09 15:45:43 +0100

    removing now useless converter code

--- Investigations/converter.cmake
+++
@@ -1,7 +0,0 @@
-set(CONVERTER ultra-converter)
-
-add_executable(${CONVERTER} converter.cpp ${CUSTOM_SOURCES})
-target_link_libraries(${CONVERTER} ad_cppgtfs)
-target_link_libraries(${CONVERTER} bz2 z expat -pthread)
-target_include_directories(${CONVERTER} PUBLIC "${CPPGTFS_INCLUDE_DIR}")
-target_include_directories(${CONVERTER} PUBLIC ${CMAKE_SOURCE_DIR})
