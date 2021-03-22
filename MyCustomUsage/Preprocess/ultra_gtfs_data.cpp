86ec2c694b72c0daee65290824907da7b638562c
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-03-22 10:31:20 +0100

    actually always checking partition consistency

--- MyCustomUsage/Preprocess/ultra_gtfs_data.cpp
+++ MyCustomUsage/Preprocess/ultra_gtfs_data.cpp
@@ -180,14 +180,12 @@ void UltraGtfsData::fromFeed(ad::cppgtfs::gtfs::Feed const& feed) {
     // prepare GTFS data :
     auto routeToTrips = partitionTripsInRoutes(feed);
 
-#ifndef NDEBUG
     bool isPartitionConsistent = my::preprocess::checkRoutePartitionConsistency(feed, routeToTrips);
     if (!isPartitionConsistent) {
         ostringstream oss;
         oss << "ERROR : number of trips after partitioning by route is not the same than number of trips in feed (=" << feed.getTrips().size() << ")";
         throw runtime_error(oss.str());
     }
-#endif
 
     auto[rankedRoutes, routeToRank] = rankRoutes(routeToTrips);
     auto[rankedStops, stopToRank] = rankStops(routeToTrips);
