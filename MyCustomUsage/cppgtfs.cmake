f194f7544b58834843503315492e3561886faa8a
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-04-21 17:47:29 +0200

    only patching file sometimes

--- MyCustomUsage/cppgtfs.cmake
+++ MyCustomUsage/cppgtfs.cmake
@@ -14,7 +14,17 @@ if(NOT cppgtfs_POPULATED)
     FetchContent_Populate(cppgtfs)
 
     # until I submit a PR with a fix, cppgtfs needs to be patched to rename pow10 :
-    execute_process(COMMAND sed -i "s@pow10@powersOf10@g" "${cppgtfs_SOURCE_DIR}/src/ad/util/CsvParser.cpp")
+    set(FILE_TO_PATCH "${cppgtfs_SOURCE_DIR}/src/ad/util/CsvParser.cpp")
+    set(DESIRED_STRING "powersOf10")
+    file(READ "${FILE_TO_PATCH}" TMP_CONTENT LIMIT 20000)
+    string(FIND "${TMP_CONTENT}" "${DESIRED_STRING}" DESIRED_STRING_INDEX)
+    if(${DESIRED_STRING_INDEX} EQUAL -1)
+        message(STATUS "Patching file : ${FILE_TO_PATCH}")
+        execute_process(COMMAND sed -i "s@pow10@${DESIRED_STRING}@g" "${FILE_TO_PATCH}")
+    else()
+        message(STATUS "No need to patch (already patched) file : ${FILE_TO_PATCH}")
+    endif()
+
 
     # Bring the populated content into the build
     add_subdirectory(${cppgtfs_SOURCE_DIR} ${cppgtfs_BINARY_DIR})
