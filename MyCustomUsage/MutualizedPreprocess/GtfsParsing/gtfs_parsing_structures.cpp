b1036cd428f96646376dadffa3dca5fcbb27573b
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-04-24 16:34:53 +0200

    trimming coordinates

--- MyCustomUsage/MutualizedPreprocess/GtfsParsing/gtfs_parsing_structures.cpp
+++ MyCustomUsage/MutualizedPreprocess/GtfsParsing/gtfs_parsing_structures.cpp
@@ -1,4 +1,5 @@
 #include <sstream>
+#include <cmath>
 
 #include "gtfs_parsing_structures.h"
 
@@ -17,8 +18,17 @@ vector<string> RouteLabel::toStopIds() const {
     return stops;
 }
 
+// there is a slight rounding error in json serialization/deserialization of coordinates
+// to make this error unvisible (and allow that using deserialized data is binary iso), we limit
+// the double places after the comma.
+// At this level of precision, there are no effects on coordinates precision.
+constexpr const double trimming_factor = 1e9;
+inline double trim(const double x) {
+    return round(x * trimming_factor) / trimming_factor;
+}
+
 ParsedStop::ParsedStop(string const& id_, string const& name_, double latitude_, double longitude_)
-    : id{id_}, name{name_}, latitude{latitude_}, longitude{longitude_} {}
+    : id{id_}, name{name_}, latitude{trim(latitude_)}, longitude{trim(longitude_)} {}
 
 string ParsedStop::as_string() const {
     ostringstream oss;
